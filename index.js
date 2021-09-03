d99ecb5de64c5cf42971b396b68d4d705ed11d99
idoco <ido@loadmill.com>
2021-09-03 22:53:28 +0300

    test redis

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const server = http.createServer(app);
 app.use(express.json());
 
 const redis = require('redis');
+const { promisify } = require("util");
+
 const client = redis.createClient(process.env.REDIS_URL);
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+const keysAsync = promisify(client.keys).bind(client);
 
 client.on("connect", function () {
     console.log('redis connect');
